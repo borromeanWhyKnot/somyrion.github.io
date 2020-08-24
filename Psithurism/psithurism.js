65f22f4222be2ff51bd4a630bc7b20b7b09b358a
Somyrion <somyrion@gmail.com>
2020-08-24 01:13:54 -0400

    Update psithurism.js

--- Psithurism/psithurism.js
+++ Psithurism/psithurism.js
@@ -64,13 +64,13 @@ var facID = "12"; // update when N-Day starts!
 			// Convert Production (P, P)
 			else if (e.keyCode == 80) {
 				if (window.location.href.indexOf("view=production") > -1) {
-					if ($('span.fancylike')[0].indexOf("Military") > -1) {
+					if ($('span.fancylike').first().indexOf("Military") > -1) {
 						$('.button[name="convertproduction"][value^="nukes"]').first().trigger('click');
 					}
-					else if ($('span.fancylike')[0].indexOf("Strategic") > -1) {
+					else if ($('span.fancylike').first().indexOf("Strategic") > -1) {
 						$('.button[name="convertproduction"][value^="shield"]').first().trigger('click');
 					}
-					else if ($('span.fancylike')[0].indexOf("Economic") > -1) {
+					else if ($('span.fancylike').first().indexOf("Economic") > -1) {
 						$('.button[name="convertproduction"][value^="shield"]').first().trigger('click');
 					}
 				}
